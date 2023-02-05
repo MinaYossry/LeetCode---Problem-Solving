666d1c8ce1f6348bd69acb0ccb4e8a2acfb4887a
Mina Yossry <mina.yossry4@gmail.com>
2023-02-05 16:14:17 +0200

    Time: 4 ms (85.12%), Space: 9.2 MB (68.93%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
-    
     int thirdMax(vector<int>& nums) {
-        set<int> numbers;
+        int max = INT_MIN;
+        int secondMax = INT_MIN;
+        int thirdMax = INT_MIN;
+        int min = INT_MAX;
 
         for (auto num: nums)
         {
-            numbers.insert(num);
-            if (numbers.size() > 3)
+            if (num < min)
+                min = num;
+
+            if (num > max)
             {
-                numbers.erase(numbers.begin());
+                thirdMax = secondMax;
+                secondMax = max;
+                max = num;
+            } else if (num > secondMax && num != max)
+            {
+                thirdMax = secondMax;
+                secondMax = num;
+            } else if (num > thirdMax && num != secondMax && num != max)
+            {
+                thirdMax = num;
             }
         }
 
-        return numbers.size() == 3 ? *numbers.begin() : *numbers.rbegin();
+        if (secondMax == thirdMax)
+            return max;
+        
+        return (thirdMax == INT_MIN && min != INT_MIN) ? max : thirdMax;
     }
 };
