c4d8000f24c08a6dd3cfb76919ebc4317cffd46d
Mina Yossry <mina.yossry4@gmail.com>
2023-02-05 22:04:40 +0200

    Time: 101 ms (48.17%), Space: 36 MB (29.09%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cs
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cs
@@ -1,32 +1,19 @@
 public class Solution {
    public IList<int> GetRow(int rowIndex)
         {
-            int[] arr0 = new int[1];
-            int[] arr1 = new int[2];
-            int[] mainArray;
-            int flag = 0;
 
+            int[] mainArray = new int[rowIndex + 1];
 
-            arr0[0] = arr1[0] = arr1[^1] = 1;
-            if ( rowIndex == 1)
-                return arr1;
-            if (rowIndex == 0)
-                return arr0;
-
-            mainArray = arr1;
-            //loop for Rows
-            for (int i=1; i < rowIndex; i++)
-            {
-                arr0 = new int[mainArray.Length + 1];
-                arr0[0] = arr0[^1] = 1;
-                
-                //loop for filling rows
-                for (int j=1;j< mainArray.Length;j++)
-                    arr0[j] = mainArray[j - 1] + mainArray[j]; 
-                
-                mainArray = arr0;
-            }
+            mainArray[0] = mainArray[^1] = 1;
        
-            return (rowIndex%2==0) ? arr0 : mainArray;
+            for (int i=1; i <= rowIndex; i++)
+            {
+                for (int j=i; j > 0; j--)
+                    mainArray[j] =  mainArray[j] + mainArray[j - 1]; 
+                
+            }
+            
+       mainArray[^1] = 1;
+            return mainArray;
         }
 }
