d21b1e8d51a3cb44b84f3790376645528caab2fc
Mina Yossry <mina.yossry4@gmail.com>
2023-02-07 13:36:43 +0200

    Time: 153 ms (41.85%), Space: 93.3 MB (91.49%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,7 +5,7 @@ public:
         int minPrice = prices.at(0);
         int maxProfit = 0;
         
-        for (int i = 0; i < prices.size(); i++)
+        for (int i = 1; i < prices.size(); i++)
         {
             minPrice = min(minPrice, prices.at(i));
             maxProfit = max(maxProfit, prices.at(i) - minPrice);
