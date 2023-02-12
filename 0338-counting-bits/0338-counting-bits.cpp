9dfc53e17f6f868833760ea225e64cba0a69e2fe
Mina Yossry <mina.yossry4@gmail.com>
2023-02-12 10:36:04 +0200

    Time: 9 ms (43.70%), Space: 7.9 MB (53.22%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -4,36 +4,34 @@ public:
         vector<int> result(n + 1);
 
         for (int i {}; i < result.size(); i++) {
-            result.at(i) = bitsCounter(i);
+            result.at(i) = result.at(i/2) + i%2;
         }
 
         return result;
     }
 
-    // int bitsCounter(int number) {
-    //     int counter {};
-    //     while (number != 0) {
-    //         counter += number & 1;
-    //         number = number >> 1;
-    //     }
-    //     return counter;
-    // }
-    
     int bitsCounter(int number) {
         int counter {};
-        
         while (number != 0) {
-            double power = log2(number);
-            
-            int num = static_cast<int>(power);
-            
-            double frac = power - num;
-            
-            counter += 1;
-        
-            number -= pow(2, num);
+            counter += number & 1;
+            number = number >> 1;
         }
-        
         return counter;
     }
+    
+//     int bitsCounter(int number) {
+//         int counter {};
+        
+//         while (number != 0) {
+//             double power = log2(number);
+            
+//             int num = static_cast<int>(power);
+                        
+//             counter += 1;
+        
+//             number -= pow(2, num);
+//         }
+        
+//         return counter;
+//     }
 };
