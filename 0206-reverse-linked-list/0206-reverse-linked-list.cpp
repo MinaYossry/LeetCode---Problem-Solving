25b03bbb14d3670d9c5e389a9a938c1000001385
Mina Yossry <mina.yossry4@gmail.com>
2023-02-13 13:35:48 +0200

    Time: 3 ms (96.77%), Space: 8.2 MB (85.74%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,33 +10,55 @@
  */
 class Solution {
 public:
+    ListNode* reverse(ListNode* start, ListNode* prev, ListNode* target)
+    {
+       if (!target)
+           return start;
+        
+        ListNode* temp = target->next;
+        target->next = start;
+        prev->next = temp;
+        
+        return reverse(target, prev, prev->next);
+    }
+    
     ListNode* reverseList(ListNode* head) {
         if (!head || !head->next)
             return head;
         
-        stack<ListNode*> nodes;
-        ListNode* trav = head;
-        while(trav)
-        {
-            nodes.push(trav);
-            trav = trav->next;
-        }
-        
-        ListNode* reversed = nodes.top();
-        nodes.pop();
-        trav = reversed;
-        
-        while(!nodes.empty())
-        {
-            trav->next = nodes.top();
-            nodes.pop();
-            
-            trav = trav->next;
-            
-        }
-        
-        trav->next = nullptr;
-        
-        return reversed;
+        return reverse(head, head, head->next);
     }
+//     ListNode* reverseList(ListNode* head) {
+        
+        
+        
+        
+//         if (!head || !head->next)
+//             return head;
+        
+//         stack<ListNode*> nodes;
+//         ListNode* trav = head;
+//         while(trav)
+//         {
+//             nodes.push(trav);
+//             trav = trav->next;
+//         }
+        
+//         ListNode* reversed = nodes.top();
+//         nodes.pop();
+//         trav = reversed;
+        
+//         while(!nodes.empty())
+//         {
+//             trav->next = nodes.top();
+//             nodes.pop();
+            
+//             trav = trav->next;
+            
+//         }
+        
+//         trav->next = nullptr;
+        
+//         return reversed;
+//     }
 };
