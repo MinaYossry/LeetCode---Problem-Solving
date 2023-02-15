cd696c15868968db478838f8cca790001096ecd3
Mina Yossry <mina.yossry4@gmail.com>
2023-02-15 19:23:06 +0200

    Time: 145 ms (29.30%), Space: 76 MB (69.73%) - LeetHub

---
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        vector<int> output(pref.size());
+        
+        output.at(0) = pref.at(0);
+        
+        for(int i = 1; i < output.size(); i++)
+        {
+            output.at(i) = pref.at(i - 1) ^ pref.at(i); 
+        }
+        
+        
+        return output;
+    }
+};
