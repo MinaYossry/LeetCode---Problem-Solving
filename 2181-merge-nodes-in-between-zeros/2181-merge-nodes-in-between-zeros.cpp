f94743d28349f81ab12719012fb025e6479b27a7
Mina Yossry <mina.yossry4@gmail.com>
2023-02-16 13:27:23 +0200

    Time: 802 ms (43.65%), Space: 274.6 MB (18.59%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -13,34 +13,57 @@ public:
     ListNode* mergeNodes(ListNode* head) {
         if (!head)
             return head;
-        ListNode* temp = head;
-        head = head->next;
-        delete temp;
-        ListNode* trav = head;
         
-        while (trav && trav->next)
+        ListNode* newHead = new ListNode(head->next->val);
+        
+        ListNode* travOld = head->next->next;
+        ListNode* travNew = newHead;
+        
+        while(travOld)
         {
-            if (trav->next && trav->next->val != 0)
+            if (travOld->val == 0 && travOld->next)
             {
-                ListNode* temp = trav->next;
-                trav->val += trav->next->val;
-                trav->next = temp->next;
-                temp->next = nullptr;
-                delete temp;
-            }
+                travNew->next = new ListNode();
+                travNew = travNew->next;
+            } 
+            else
+                travNew->val += travOld->val;
             
-            if (trav->next && trav->next->val == 0 && !trav->next->next)
-            {
-                delete trav->next;
-                trav->next = nullptr;
-            }
-               
-            
-            else if (trav->next && trav->next->val == 0)
-                trav = trav->next;
-            
+            travOld = travOld->next;
         }
         
-        return head;
+        return newHead;
+//         if (!head)
+//             return head;
+        
+//         ListNode* temp = head;
+//         head = head->next;
+//         delete temp;
+//         ListNode* trav = head;
+        
+//         while (trav && trav->next)
+//         {
+//             if (trav->next && trav->next->val != 0)
+//             {
+//                 ListNode* temp = trav->next;
+//                 trav->val += trav->next->val;
+//                 trav->next = temp->next;
+//                 temp->next = nullptr;
+//                 delete temp;
+//             }
+            
+//             if (trav->next && trav->next->val == 0 && !trav->next->next)
+//             {
+//                 delete trav->next;
+//                 trav->next = nullptr;
+//             }
+               
+            
+//             else if (trav->next && trav->next->val == 0)
+//                 trav = trav->next;
+            
+//         }
+        
+//         return head;
     }
 };
