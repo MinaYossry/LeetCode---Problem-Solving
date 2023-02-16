de240b5e56542390887ada20b0a8cae9c1c46c54
Mina Yossry <mina.yossry4@gmail.com>
2023-02-16 13:53:33 +0200

    Time: 10 ms (56.30%), Space: 10.1 MB (79.19%) - LeetHub

--- 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.cpp
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.cpp
@@ -4,21 +4,13 @@ public:
         vector<int> maxRows (grid.size());
         vector<int> maxCols (grid.size());
         
-        for(int i = 0; i < grid.size(); i++)
+        for(int row = 0; row < grid.size(); row++)
         {
-            int max = *(max_element(grid.at(i).begin(), grid.at(i).end()));
-            maxRows.at(i) = max;
-        }
-        
-        for(int col = 0; col < grid.size(); col++)
-        {
-            int max = grid.at(0).at(col);
-            for (int row = 0; row < grid.size(); row++)
+            for (int col = 0; col < grid.size(); col++)
             {
-                if (grid.at(row).at(col) > max)
-                    max = grid.at(row).at(col);
+                maxRows.at(row) = max(maxRows.at(row), grid.at(row).at(col));
+                maxCols.at(col) = max(maxCols.at(col), grid.at(row).at(col));
             }
-            maxCols.at(col) = max;
         }
         
         int result = 0;
