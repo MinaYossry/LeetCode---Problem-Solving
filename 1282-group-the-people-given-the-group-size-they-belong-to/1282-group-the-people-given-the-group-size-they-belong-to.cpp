f2a607c97aee3029807e7e5c14ef36689efe747e
Mina Yossry <mina.yossry4@gmail.com>
2023-02-18 12:45:16 +0200

    Time: 7 ms (96.61%), Space: 12.9 MB (71.11%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -8,12 +8,8 @@ public:
             mapping[groupSizes.at(i)].push_back(i);
         
         for(const auto &pair: mapping)
-        {
             for (int i = 0; i < pair.second.size(); i += pair.first)
-            {
                 result.emplace_back(pair.second.begin() + i, pair.second.begin() + i + pair.first);
-            }
-        }
         
         return result;
     }
