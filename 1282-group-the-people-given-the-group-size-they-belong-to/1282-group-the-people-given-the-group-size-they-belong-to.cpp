5128c2f9e928f943fcb55f2a37d00dd18830e320
Mina Yossry <mina.yossry4@gmail.com>
2023-02-18 12:45:05 +0200

    Time: 7 ms (96.61%), Space: 12.8 MB (73.68%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        unordered_map<int, vector<int>> mapping;
+        vector<vector<int>> result;
+        
+        for(int i = 0; i < groupSizes.size(); i++)
+            mapping[groupSizes.at(i)].push_back(i);
+        
+        for(const auto &pair: mapping)
+        {
+            for (int i = 0; i < pair.second.size(); i += pair.first)
+            {
+                result.emplace_back(pair.second.begin() + i, pair.second.begin() + i + pair.first);
+            }
+        }
+        
+        return result;
+    }
+};
